315caa955c98bc50d8d58e5ec394899dcd79bc95
Mikhail Diatchenko <mikhail@webgear.co.nz>
2012-03-15 14:38:43 +1300

    Added project harness

---
+++ Soyuz5.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace System
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Rounds time with seconds precision (removing milliseonds and ticks)
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static DateTime RoundToSecond(this DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second);
+        }
+
+        /// <summary>
+        /// Rounds time with minutes precision (removing seconds, milliseonds and ticks)
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static DateTime RoundToMinute(this DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0);
+        }
+
+        /// <summary>
+        /// Gets Monday of the week that the date falls on. Discards time component.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static DateTime GetMondayOfWeek(this DateTime date)
+        {
+            //convert from sun=0 & sat=6 to mon=0 & sun=6
+            int dayOfTheWeek = (int)date.DayOfWeek;
+            if (dayOfTheWeek == 0)
+                dayOfTheWeek = 7;
+            dayOfTheWeek--; //here we have mon=0 & sun=6
+            return date.Date.AddDays(-dayOfTheWeek);
+        }
+
+        /// <summary>
+        /// Gets first date of the month that the date. Discards time component.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static DateTime GetFirstDayOfMonth(this DateTime date)
+        {
+            return date.Date.AddDays(1 - date.Day);
+        }
+    }
+}
---
+++ Soyuz5.Extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Soyuz5.Extensions")]
+[assembly: AssemblyDescription("Swiss army knife for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Soyuz5")]
+[assembly: AssemblyProduct("Soyuz5.Extensions")]
+[assembly: AssemblyCopyright("Copyright © Soyuz5 2010-2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0c9e5b89-6311-4996-bec6-667c36c36e4f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
