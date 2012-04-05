ad3dd5f731c3cb6073e63310d87d46e76a0c2003
Mikhail Diatchenko <mikhail@webgear.co.nz>
2012-04-06 09:55:58 +1200

    Added ParseEnum extension method with default value.

--- Soyuz5.Extensions/EnumExtensions.cs
+++ Soyuz5.Extensions/EnumExtensions.cs
@@ -2,6 +2,22 @@
 {
     public static class EnumExtensions
     {
-        
+        /// <summary>
+        /// Tries to parse inuu string as Enum of specified type. If unsuccessful returns default value.
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="input"></param>
+        /// <param name="defaultValue"></param>
+        /// <param name="ignoreCase">Default false</param>
+        /// <returns></returns>
+        public static TEnum ParseEnum<TEnum>(this string input, TEnum defaultValue, bool ignoreCase = false)
+             where TEnum : struct
+        {
+            TEnum value;
+            if (Enum.TryParse<TEnum>(input, true, out value))
+                return value;
+
+            return defaultValue;
+        }
     }
 }
