783665f9131f2db9d51af5a3881972fd22a84331
Mikhail Diatchenko <mikhail@webgear.co.nz>
2012-07-16 10:44:33 +1200

    Added enum helper to get enum value description from DescriptionAttribute and to get a list of value/label pairs for enum type.

--- Soyuz5.Extensions.Tests/EnumExtensionsTests.cs
+++ Soyuz5.Extensions.Tests/EnumExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 
@@ -12,9 +13,19 @@ namespace Soyuz5.Extensions.Tests
         {
             None = 0,
             One = 1,
+            [System.ComponentModel.Description("2")]
             Two = 2,
             Four = 4,
             All = One | Two | Four
+        }
+
+        public enum MyEnum
+        {
+            None = 0,
+            One = 1,
+            [System.ComponentModel.Description("2")]
+            Two = 2,
+            Three = 3
         }
 
         [Test]
@@ -43,6 +54,82 @@ namespace Soyuz5.Extensions.Tests
         public void GetFlags_All()
         {
             Assert.AreEqual(4, MyFlags.All.GetFlags().Count());
+        }
+
+        [Test]
+        public void GetEnumDescription_Default()
+        {
+            Assert.AreEqual("One", MyFlags.One.GetEnumDescription());
+        }
+
+        [Test]
+        public void GetEnumDescription_DescriptionAttribute()
+        {
+            Assert.AreEqual("2", MyFlags.Two.GetEnumDescription());
+        }
+
+        [Test]
+        public void GetEnumDescription_Flags()
+        {
+            MyFlags flags = (MyFlags.One | MyFlags.Two);
+            Assert.AreEqual("One, 2", flags.GetEnumDescription());
+        }
+
+        [Test]
+        public void GetEnumDescription_NonStandard()
+        {
+            MyFlags flags = (MyFlags) 14;
+            Assert.AreEqual("2, Four", flags.GetEnumDescription());
+        }
+
+        [Test]
+        public void GetEnumDescription_NoFLags()
+        {
+            MyEnum value = (MyEnum)14;
+            Assert.AreEqual("2", value.GetEnumDescription());
+
+            value = (MyEnum)4;
+            Assert.AreEqual("", value.GetEnumDescription());
+
+            value = (MyEnum)3;
+            Assert.AreEqual("Three", value.GetEnumDescription());
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void GetEnumBindableList_NotEnum()
+        {
+            IList<KeyValuePair<int, string>> list = typeof(int).GetEnumBindableList();
+        }
+
+        [Test]
+        public void GetEnumBindableList()
+        {
+            IList<KeyValuePair<int, string>> list = typeof (MyEnum).GetEnumBindableList();
+            Assert.AreEqual(4, list.Count);
+            Assert.AreEqual("None", list[0].Value);
+            Assert.AreEqual("One", list[1].Value);
+            Assert.AreEqual("2", list[2].Value);
+            Assert.AreEqual("Three", list[3].Value);
+            Assert.AreEqual(0, list[0].Key);
+            Assert.AreEqual(1, list[1].Key);
+            Assert.AreEqual(2, list[2].Key);
+            Assert.AreEqual(3, list[3].Key);
+        }
+
+        [Test]
+        public void GetEnumBindableList_Generic()
+        {
+            IList<KeyValuePair<MyEnum, string>> list = typeof (MyEnum).GetEnumBindableList<MyEnum>();
+            Assert.AreEqual(4, list.Count);
+            Assert.AreEqual("None", list[0].Value);
+            Assert.AreEqual("One", list[1].Value);
+            Assert.AreEqual("2", list[2].Value);
+            Assert.AreEqual("Three", list[3].Value);
+            Assert.AreEqual(MyEnum.None, list[0].Key);
+            Assert.AreEqual(MyEnum.One, list[1].Key);
+            Assert.AreEqual(MyEnum.Two, list[2].Key);
+            Assert.AreEqual(MyEnum.Three, list[3].Key);
         }
     }
 }
--- Soyuz5.Extensions/EnumExtensions.cs
+++ Soyuz5.Extensions/EnumExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
 
 namespace System
 {
@@ -37,6 +40,125 @@ namespace System
                 if (flags.HasFlag(value))
                     yield return value;
             }
+        }
+/*
+        /// <summary>
+        /// Gets an enumerable sequence of all values that are present in flags.
+        /// </summary>
+        /// <param name="flags"></param>
+        /// <returns></returns>
+        public static IEnumerable<TEnum> GetFlags<TEnum>(this TEnum flags)
+        {
+            ulong intFlags = Convert.ToUInt64(flags);
+            foreach (TEnum value in Enum.GetValues(flags.GetType()))
+            {
+                ulong intValue = Convert.ToUInt64(value);
+                if (intValue == 0)
+                    continue;
+
+                if ((intFlags & intValue) == intValue)
+                    yield return value;
+            }
+        }*/
+
+        /// <summary>
+        /// Gets Description attribute value for an enum value if available. Otherwise returns enum name. 
+        /// If value is flags will returns a list of flags (will use description if set via DescriptionAttribute) that are set in value. 
+        /// If is not a standard value and not flags will find ine matchin value and returns its desription (in available). 
+        /// If no match - will return empty string.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string GetEnumDescription(this Enum value)
+        {
+            Type enumType = value.GetType();
+            string valueName = Enum.GetName(enumType, value);
+            if (valueName == null)
+            {
+                // non-standard or multiple values
+                return string.Join(", ", value.GetFlags().Select(f => f.GetEnumDescription()).ToArray());
+            }
+            MemberInfo[] members = enumType.GetMember(valueName);
+            if (members.Length > 0)
+            {
+                DescriptionAttribute attr = (DescriptionAttribute)Attribute.GetCustomAttribute(members[0], typeof(DescriptionAttribute));
+                if (attr != null && !string.IsNullOrEmpty(attr.Description))
+                    return attr.Description;
+            }
+            return Enum.GetName(enumType, value);
+        }
+
+        /// <summary>
+        /// Returns list of value/name pairs for all available enum values. 
+        /// Key contains an integer representation of enum value and value is the name or description (set via DescriptionAttribute)
+        /// </summary>
+        /// <param name="enumType"></param>
+        /// <param name="useDescription">If true will use value desription if available</param>
+        /// <returns></returns>
+        public static IList<KeyValuePair<int, string>> GetEnumBindableList(this Type enumType, bool useDescription = true)
+        {
+            if (!enumType.IsEnum)
+                throw new ArgumentOutOfRangeException("enumType", "Must be Enum");
+
+            var list = new List<KeyValuePair<int, string>>();
+            foreach (Enum value in Enum.GetValues(enumType))
+            {
+                if (useDescription)
+                {
+                    list.Add(new KeyValuePair<int, string>(Convert.ToInt32(value), value.GetEnumDescription()));
+                }
+                else
+                {
+                    list.Add(new KeyValuePair<int, string>(Convert.ToInt32(value), Enum.GetName(enumType, value)));
+                }
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Returns list of value/name pairs for all available enum values. 
+        /// Key contains enum value casted to T and value is the name or description (set via DescriptionAttribute). 
+        /// </summary>
+        /// <param name="enumType"></param>
+        /// <param name="useDescription">If true will use value desription if available</param>
+        /// <returns></returns>
+        public static IList<KeyValuePair<T, string>> GetEnumBindableList<T>(this Type enumType, bool useDescription = true)
+            where T : struct
+        {
+            if (!enumType.IsEnum)
+                throw new ArgumentOutOfRangeException("enumType", "Must be Enum");
+
+            var list = new List<KeyValuePair<T, string>>();
+            foreach (Enum value in Enum.GetValues(enumType))
+            {
+
+                if (useDescription)
+                {
+                    list.Add(new KeyValuePair<T, string>((T)(object)value, value.GetEnumDescription()));
+                }
+                else
+                {
+                    list.Add(new KeyValuePair<T, string>((T)(object)value, Enum.GetName(enumType, value)));
+                }
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Count number of flags in enum. 
+        /// A more readable version could be written based on http://www.dotnetperls.com/bitcount http://gurmeet.net/puzzles/fast-bit-counting-routines/
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static UInt32 CountFlags<T>(this T enumValue)
+            where T : struct
+        {
+            UInt32 v = Convert.ToUInt32(enumValue);
+            v = v - ((v >> 1) & 0x55555555); // reuse input as temporary
+            v = (v & 0x33333333) + ((v >> 2) & 0x33333333); // temp
+            UInt32 c = ((v + (v >> 4) & 0xF0F0F0F) * 0x1010101) >> 24; // count
+            return c;
         }
     }
 }
