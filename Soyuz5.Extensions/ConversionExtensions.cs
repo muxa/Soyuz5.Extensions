c4fdf63affbec8d87c06247a8378c7fe1d3f16df
Mikhail Diatchenko <mikhail@webgear.co.nz>
2012-10-30 12:23:43 +1300

    Added Parse methods for nullable numbers.

--- Soyuz5.Extensions/ConversionExtensions.cs
+++ Soyuz5.Extensions/ConversionExtensions.cs
@@ -26,6 +26,22 @@
         }
 
         /// <summary>
+        /// Tries to parse input string as int. If unsuccessful returns default value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static int? Parse(this string value, int? defaultValue = null)
+        {
+            int result;
+
+            if (int.TryParse(value, out result))
+                return result;
+
+            return defaultValue;
+        }
+
+        /// <summary>
         /// Tries to parse input string as decimal. If unsuccessful returns default value.
         /// </summary>
         /// <param name="value"></param>
@@ -42,12 +58,44 @@
         }
 
         /// <summary>
+        /// Tries to parse input string as decimal. If unsuccessful returns default value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static decimal? Parse(this string value, decimal? defaultValue = null)
+        {
+            decimal result;
+
+            if (decimal.TryParse(value, out result))
+                return result;
+
+            return defaultValue;
+        }
+
+        /// <summary>
         /// Tries to parse input string as double. If unsuccessful returns default value.
         /// </summary>
         /// <param name="value"></param>
         /// <param name="defaultValue"></param>
         /// <returns></returns>
         public static double Parse(this string value, double defaultValue)
+        {
+            double result;
+
+            if (double.TryParse(value, out result))
+                return result;
+
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to parse input string as double. If unsuccessful returns default value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static double? Parse(this string value, double? defaultValue = null)
         {
             double result;
 
