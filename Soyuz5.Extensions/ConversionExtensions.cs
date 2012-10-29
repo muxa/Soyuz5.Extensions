a944a999bcc99de5cc6feee41156500e63a71e71
Mikhail Diatchenko <mikhail@webgear.co.nz>
2012-10-30 12:18:09 +1300

    Added Parse methods for decimal and double

--- Soyuz5.Extensions/ConversionExtensions.cs
+++ Soyuz5.Extensions/ConversionExtensions.cs
@@ -23,6 +23,38 @@
                 return result;
 
             return defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to parse input string as decimal. If unsuccessful returns default value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static decimal Parse(this string value, decimal defaultValue)
+        {
+            decimal result;
+
+            if (decimal.TryParse(value, out result))
+                return result;
+
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to parse input string as double. If unsuccessful returns default value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static double Parse(this string value, double defaultValue)
+        {
+            double result;
+
+            if (double.TryParse(value, out result))
+                return result;
+
+            return defaultValue;
         } 
     }
 }
